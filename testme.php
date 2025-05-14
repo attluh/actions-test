1730caa55133d31e059a7c3d7ef60ed74330411e
attluh <attluh@gmail.com>
2025-05-14 13:47:04 -0600

    add code that should result in phpcs warnings

--- testme.php
+++ testme.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Update this file and PR to main to run the phpcs Github Action." . PHP_EOL;
+// Update this file and PR to main to run the phpcs Github Action.
+
+function defyTheStandard() {
+    return "This function is a new symbol declaration." . PHP_EOL;
+}
+
+echo ${defyTheStandard()};
+echo "Oops, that statement was a side effect." . PHP_EOL;
